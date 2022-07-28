e3b2681e067ce13b21fd3b2f964c866c4d8fed65
Ahsan <achsan.ajr@gmail.com>
2022-07-28 17:09:46 +0700

    penambahan 404 dan link

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <div className={styles.container}>
-        <div className="shadow-lg max-w-xl bg-slate-200 rounded m-auto my-7 p-5">
+        <div className="shadow-lg max-w-xl bg-slate-100 rounded m-auto mt-7 p-5">
           <img
             src="/favicon.ico"
             alt="Ahsan Kejepangan"
@@ -23,7 +23,7 @@ export default function Home() {
             Membantu kamu untuk belajar bahasa Jepang dengan asik.
           </p>
           <div id="links" className="mt-8">
-            <div className="text-center mt-3 bg-slate-300 rounded hover:bg-slate-400">
+            <div className="text-center mt-3 bg-orange-200 rounded hover:bg-orange-300">
               <button
                 onClick={() => Router.push("/ebook")}
                 className="p-3 w-full"
@@ -31,7 +31,7 @@ export default function Home() {
                 Paket Ebook Kejepangan
               </button>
             </div>
-            <div className="text-center mt-3 bg-slate-300 rounded hover:bg-slate-400">
+            <div className="text-center mt-3 bg-orange-200 rounded hover:bg-orange-300">
               <a
                 href="https://bit.ly/gotradeAhsan"
                 target="_blank"
@@ -40,6 +40,92 @@ export default function Home() {
                 <p className="p-3 w-full">
                   Mulai Investadi di Gotrade Indonesia Mulai dari $1
                 </p>
+              </a>
+            </div>
+            <div className="text-center mt-3 bg-orange-200 rounded hover:bg-orange-300">
+              <a
+                href="https://contentacademy.id/member/aff/go/seomaulana?i=5"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p className="p-3 w-full">
+                  Belajar Menjadi Konten Kreator di Instagram
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="shadow-lg max-w-xl bg-slate-200 rounded mt-2 mb-7 m-auto p-5">
+          <div id="sosmed">
+            <div className="my-3 text-center">
+              <img
+                src="/IG.png"
+                alt="Instagram"
+                width={50}
+                className="block m-auto"
+              />
+              <p className="font-bold">Instagram</p>
+              <p className="font-bold">@kejepangan</p>
+              <div className="text-center mt-3 bg-orange-700 text-white rounded hover:bg-orange-800 max-w-xs block m-auto">
+                <a
+                  href="https://instagram.com/kejepangan"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="p-3 w-full">Follow</p>
+                </a>
+              </div>
+            </div>
+            <div className="my-3 text-center">
+              <img
+                src="/TikTok.png"
+                alt="TikTok"
+                width={50}
+                className="block m-auto"
+              />
+              <p className="font-bold">TikTok</p>
+              <p className="font-bold">@kejepangan</p>
+              <div className="text-center mt-3 bg-slate-700 text-white rounded hover:bg-slate-800 max-w-xs block m-auto">
+                <a
+                  href="https://www.tiktok.com/@kejepangan"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="p-3 w-full">Follow</p>
+                </a>
+              </div>
+            </div>
+            <div className="my-3 text-center">
+              <img
+                src="/Youtube.png"
+                alt="Youtube"
+                width={50}
+                className="block m-auto"
+              />
+              <p className="font-bold">Youtube</p>
+              <p className="font-bold">Kejepangan</p>
+              <div className="text-center mt-3 bg-red-700 text-white rounded hover:bg-red-800 max-w-xs block m-auto">
+                <a
+                  href="https://www.youtube.com/channel/UCZfT07x6pRpjwecfw3TJK_A"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="p-3 w-full">Subscribe</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="kerjasama">
+            <p className="text-slate-600 mt-5 text-center">
+              Untuk Brand Endorsement, Webinar, Pemateri Klik disini:
+            </p>
+            <div className="text-center mt-3 bg-green-700 text-white rounded hover:bg-green-800 max-w-xs block m-auto">
+              <a
+                href="https://wa.me/6285855997747?text=Hello%20kak%2C%20saya%20mau%20mengajukan%20kerjasama%20dengan%20%40kejepangan"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p className="p-3 w-full">Chat Manager Ahsan</p>
               </a>
             </div>
           </div>
